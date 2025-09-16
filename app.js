3baf1d5af85c04c4adbec6528ae5796d3e20daf0
SAMANTHA PARKES <slmillward@outlook.com>
2025-09-16 16:03:58 +0100

    npm

---
+++ app.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Hello world!"));
+console.log(chalk.green("Hello world!"));
+console.log(chalk.yellow("Hello world!"));
+console.log(chalk.red("Hello world!"));
